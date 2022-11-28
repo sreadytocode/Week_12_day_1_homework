b68dcccfe4fc4626a73f7049b81f1198095e0220
sreadytocode <sara.younis2303@gmail.com>
2022-11-28 22:33:22 +0000

    Finished first task planets in task_1 folder. Made second folder to store task_2

---
+++ task_1/Planet_mini_lab_homework/src/main/java/Planet.java
@@ -0,0 +1,21 @@
+public class Planet {
+    private String name;
+    private int size;
+
+    public Planet(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+   public String explode() {
+        return "Boom! " + this.name + " has exploded.";
+   }
+}
---
+++ task_1/Planet_mini_lab_homework/src/main/java/Runner.java
@@ -0,0 +1,7 @@
+public class Runner {
+    public static void main(String[] args) {
+        Planet mars = new Planet("Mars", 908973);
+        System.out.println(mars.getName() + " is " + mars.getSize());
+        System.out.println(mars.explode());
+    }
+}
